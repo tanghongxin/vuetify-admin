d980ea6bd2a2e36397df680f5dc382c27bba1ab8
tanghongxin <hongxin.tang@hotmail.com>
2019-11-30 11:52:44 +0800

    feat: git cz 工具引入

---
+++ .cz-config.js
@@ -0,0 +1,56 @@
+module.exports = {
+  types: [
+    {
+      value: 'feat',
+      name: 'feat:     新增功能',
+    },
+    {
+      value: 'fix',
+      name: 'fix:      bug 修复',
+    },
+    {
+      value: 'style',
+      name: 'style:    不影响程序逻辑的代码修改（代码缩进、空白字符等）',
+    },
+    {
+      value: 'chore',
+      name: 'chore:    其他类型（杂项）',
+    },
+    {
+      value: 'merge',
+      name: 'merge:    分支合并',
+    },
+    {
+      value: 'WIP',
+      name: 'WIP:      仍在进行中',
+    },
+    {
+      value: 'perf',
+      name: 'perf:     性能、代码可读性改进',
+    },
+    {
+      value: 'docs',
+      name: 'docs:     文档更新',
+    },
+    {
+      value: 'refactor',
+      name: 'refactor: 重构',
+    },
+    {
+      value: 'test',
+      name: 'test:     新增测试用例或是更新现有测试',
+    },
+    {
+      value: 'revert',
+      name: 'revert:   回滚',
+    },
+    {
+      value: 'build',
+      name: 'build:    构建、打包、部署配置修改',
+    },
+    {
+      value: 'ci',
+      name: 'ci:       持续集成配置（GitLab等）',
+    },
+  ],
+}
