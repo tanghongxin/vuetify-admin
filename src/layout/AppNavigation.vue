461f8db2779dfcb2d1f5c826e4f86f7ffed1d572
tanghongxin <hongxin.tang@hotmail.com>
2021-07-03 13:42:13 +0800

    🎨 (AppNavigation) 优化冗余代码

--- src/layout/AppNavigation.vue
+++ src/layout/AppNavigation.vue
@@ -1,62 +1,29 @@
-<template>
-  <v-navigation-drawer
-    app
-    :clipped="appPermanentNavigation || $vuetify.breakpoint.lgAndUp"
-    :value="appPermanentNavigation || appNavigation"
-    :permanent="appPermanentNavigation"
-    @input="handleInput"
-    ref="drawer"
-  >
-    <RecursiveMenus :items="menus" />
-  </v-navigation-drawer>
-</template>
-
 <script>
 import RecursiveMenus from './RecursiveMenus.vue'
 import mixin from './mixin.vue'
 
 export default {
   name:'AppNavigation',
-  components: {
-    RecursiveMenus,
-  },
   mixins: [mixin],
-  computed: {
-    menus () {
-      return this.recursive(this.$store.state.account.menus)
-    },
-  },
-  methods: {
-    recursive (items) {
-      return items.map(item => {
-        const menu = {
-          hidden: !!item.hidden,
-          icon: item.icon || '',
-          permissions: item.permissions || [],
-          text: item.text || '',
-          to: item.to || '',
-          type: item.type || '',
-        }
-        switch (item.type) {
-          case 'MENU':
-            Object.assign(menu, {
-              children: this.recursive(item.children || []),
-            })
-            break
-          case 'VIEW':
-          default:
-            break
-        }
-
-        return menu
-      })
-    },
-    handleInput () {
-      // HACK
-      if (this.$refs.drawer.isActive !== this.appNavigation) {
-        this.toggleAppNavigation()
-      }
-    },
+  render () {
+    const { 
+      $store, $vuetify, appPermanentNavigation,
+      appNavigation, $refs, toggleAppNavigation,
+    } = this
+    return (
+      <v-navigation-drawer
+        app
+        clipped={appPermanentNavigation || $vuetify.breakpoint.lgAndUp}
+        value={appPermanentNavigation || appNavigation}
+        permanent={appPermanentNavigation}
+        ref="drawer"
+        onInput={() => {
+          $refs.drawer.isActive !== appNavigation && toggleAppNavigation()
+        }}
+      >
+        <RecursiveMenus items={$store.state.account.menus} />
+      </v-navigation-drawer>
+    )
   },
 }
 </script>
