d6ced76c55ef4f2d99bc0eb4c3b897f722ce8574
tanghongxin <hongxin.tang@hotmail.com>
2021-07-05 16:16:06 +0800

    :recycle: (DateTable) 使用 template 取代 render 已获得更好可读性

--- src/components/DataTable/index.vue
+++ src/components/DataTable/index.vue
@@ -1,6 +1,55 @@
+<template>
+  <div class="data-table fill-width fill-height d-flex flex-column">
+    <slot name="search" />
+
+    <div class="d-flex flex-row pb-1 px-2">
+      <slot name="actions" />
+      <v-spacer />
+      <v-btn class="mr-2" depressed tile @click="emit({ page: 1 })">
+        查询
+      </v-btn>
+      <v-btn depressed tile @click="emit({})">
+        刷新
+      </v-btn>
+    </div>
+
+    <div class="flex-grow-1" :style="{ position: 'relative' }">
+      <v-data-table
+        class="elevation-0"
+        fixed-header
+        :headers="headers"
+        :hide-default-footer="false"
+        :footer-props="{
+          itemsPerPageText: '每页条数',
+          itemsPerPageOptions: [15, 20, 30, 50],
+          showCurrentPage: true,
+          showFirstLastPage: true,
+        }"
+        :items="items"
+        :item-key="itemKey"
+        locale="zh-cn"
+        :options="options"
+        :server-items-length="options.total || 0"
+        :no-data-text="loading ? '加载中...' : '暂无数据'"
+        @update:options="emit($event)"
+      >
+        <template v-for="(_, slot) of $scopedSlots" v-slot:[slot]="scope">
+          <slot :name="slot" v-bind="scope" />
+        </template>
+      </v-data-table>
+      <VLoading absolute :value="loading" />
+    </div>
+  </div>
+</template>
+
 <script>
+import VLoading from '@/components/VImplements/VLoading.vue'
+
 export default {
   name: 'DataTable',
+  components: {
+    VLoading,
+  },
   props: {
     headers: {
       type: Array,
@@ -27,122 +76,26 @@ export default {
       default: () => ({}),
     },
   },
-  render (h) {
-    // 搜索栏
-    const search =
-      <div>{ this.$slots.search }</div>
-    // 工具栏
-    const actions =
-      <div class="d-flex flex-row pb-1 px-2">
-        { this.$slots.actions }
-        <v-spacer />
-        <v-btn
-          class="mr-2"
-          depressed 
-          tile 
-          onClick={ this.handleSearch }
-        >
-        查询
-        </v-btn>
-
-        <v-btn
-          depressed 
-          tile 
-          onClick={ this.handleRefresh }
-        >
-          刷新
-        </v-btn>
-      </div>
-    const loading = <v-overlay
-      absolute
-      color="rgba(255, 255, 255, 0.7)"
-      opacity="1"
-      value={ this.loading }
-    >
-      <v-progress-circular
-        color="primary"
-        indeterminate
-        size="64"
-      />
-    </v-overlay>
-    // 表格
-    const table = h('v-data-table', {
-      class: 'elevation-0',
-      props: {
-        fixedHeader: true,
-        headers: this.headers,
-        hideDefaultFooter: false,
-        footerProps: {
-          itemsPerPageText: '每页条数',
-          // itemsPerPageAllText: '全部',
-          itemsPerPageOptions: [10, 15, 20, 50],
-          showCurrentPage: true,
-          showFirstLastPage: true,
-        },
-        items: this.items,
-        itemKey: this.itemKey,
-        locale: 'zh-cn',
-        options: this.options,
-        serverItemsLength: this.options.total || 0,
-        // loading: this.loading,
-        noDataText: this.loading ? '加载中...' : '暂无数据',
-      },
-      on: {
-        'update:options': this.handleTableChange,
-      },
-      scopedSlots: {
-        ...this.$scopedSlots,
-        // 'footer.page-text': ({ itemsLength }) => `共 ${itemsLength} 条记录`,
-      },
-    })
-    const defaultSlot = <div>{ this.$slots.default }</div>
-    return <div class="DateTable fill-width fill-height d-flex flex-column">
-      { search }
-      { actions }
-      <div style="position: relative" class="flex-grow-1">
-        { table }
-        { loading }
-      </div>
-      { defaultSlot }
-    </div>
-  },
-  data: () => ({}),
   methods: {
-    handleSearch () {
-      this.$emit('update:options', {
-        ...this.options,
-        page: 1,
-      })
-    },
-    handleRefresh () {
-      this.$emit('update:options', {
-        ...this.options,
-      })
-    },
-    handleTableChange (options) {
-      this.$emit('update:options', {
-        ...this.options,
-        ...options,
-      })
+    emit (payload = {}) {
+      this.$emit('update:options', Object.assign({}, this.options, payload))
     },
   },
 }
 </script>
 
 <style lang="scss">
-.DateTable {
+.data-table {
   position: static !important;
-  width: 100%;
-  height: 100%;
 
   .v-pagination {
-    width: auto !important;
     text-align: right !important;
+    width: auto !important;
   }
 
   .v-toolbar__content {
-    padding-top: 0;
     padding-bottom: 0;
+    padding-top: 0;
   }
 
   .v-data-footer {
@@ -151,6 +104,7 @@ export default {
 
   th,
   td {
+    /* stylelint-disable-next-line */
     @extend .text-no-wrap !optional;
   }
 }
