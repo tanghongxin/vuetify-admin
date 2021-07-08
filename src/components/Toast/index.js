f68a47871629bb1ba5a2aff972b9c0fb8d4663f4
tanghongxin <hongxin.tang@hotmail.com>
2021-07-08 11:34:18 +0800

    chore (Toast) replace duplicated functions with closure

--- src/components/Toast/index.js
+++ src/components/Toast/index.js
@@ -1,11 +1,13 @@
 import Toast from './Toast'
 import event, { EVENT_TYPE_ADD_ITEM } from './event'
 
+const add = (color) => ({ message = '', timeout = 4000 }) => event.emit(EVENT_TYPE_ADD_ITEM, { message, timeout, color })
+
 const toast = {
-  success: ({ message = '', timeout = 4000 }) => event.emit(EVENT_TYPE_ADD_ITEM, { message, timeout, color: 'success' }),
-  warning: ({ message = '', timeout = 4000 }) => event.emit(EVENT_TYPE_ADD_ITEM, { message, timeout, color: 'warning' }),
-  error: ({ message = '', timeout = 4000 }) => event.emit((EVENT_TYPE_ADD_ITEM, { message, timeout, color: 'error' })),
-  info: ({ message = '', timeout = 4000 }) => event.emit(EVENT_TYPE_ADD_ITEM, { message, timeout, color: 'info' }),
+  success: add('success'),
+  warning: add('warning'),
+  error: add('error'),
+  info: add('info'),
 }
 
 export  {
