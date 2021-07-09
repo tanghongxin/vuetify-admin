3e95528f1e6da721fb7eff586043fa8d2177abe5
tanghongxin <hongxin.tang@hotmail.com>
2021-07-09 23:23:49 +0800

    💄  (VLoading) adapt dark mode

--- src/components/VImplements/VLoading.vue
+++ src/components/VImplements/VLoading.vue
@@ -1,6 +1,6 @@
 <template>
   <v-overlay
-    :color="value ? 'rgba(255, 255, 255, 0.7)' : 'transparent'"
+    :color="value ? color : 'transparent'"
     opacity="1"
     v-bind="$props"
   >
@@ -24,6 +24,11 @@ export default {
     absolute: {
       type: Boolean,
       default: false,
+    },
+  },
+  computed: {
+    color () {
+      return this.$vuetify.theme.dark ? 'rgba(0, 0, 0, 0.7)' : 'rgba(255, 255, 255, 0.7)'
     },
   },
 }
