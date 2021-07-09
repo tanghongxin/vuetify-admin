2c4704b3ad53f412906d4f127bc1d130106c8089
tanghongxin <hongxin.tang@hotmail.com>
2021-07-09 22:32:22 +0800

    🚧 DataTable support fixed columns

--- src/components/DataTable/index.vue
+++ src/components/DataTable/index.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="data-table fill-width fill-height d-flex flex-column">
+    <css-style>
+      {{ fixedColumnStyle }}
+    </css-style>
+
     <slot name="search" />
 
     <div class="d-flex flex-row pb-1 px-2">
@@ -50,6 +54,11 @@ export default {
   name: 'DataTable',
   components: {
     VLoading,
+    CssStyle: {
+      render (h) {
+        return h('style', this.$slots.default)
+      },
+    },
   },
   props: {
     headers: {
@@ -92,6 +101,50 @@ export default {
     originalOptions () {
       const { page, itemsPerPage, sortBy, sortDesc } = this.options
       return { page, itemsPerPage, sortBy, sortDesc }
+    },
+    fixedColumnStyle () {
+      // TODO: analyze items props
+      const fixedColsL = [1]
+      const fixedColsR = [1]
+
+      return [
+        ...fixedColsL.map(col => `
+        .data-table tr:not(.v-data-table__mobile-table-row) > td:nth-child(${col}),
+        .data-table thead:not(.v-data-table-header-mobile) tr > th:nth-child(${col}) {
+          background: #fff;
+          position: sticky !important;
+          left: 0;
+          z-index: 2;
+        }
+
+        .data-table tr:not(.v-data-table__mobile-table-row):hover > td:nth-child(${col}),
+        .data-table thead:not(.v-data-table-header-mobile) tr:hover > th:nth-child(${col}) {
+          background: inherit;
+        }
+
+        .data-table thead:not(.v-data-table-header-mobile) > tr > th:nth-child(${col}) {
+          z-index: 3 !important;
+        }
+      `),
+        ...fixedColsR.map(col => `
+        .data-table tr:not(.v-data-table__mobile-table-row) > td:nth-last-child(${col}),
+        .data-table thead:not(.v-data-table-header-mobile) tr > th:nth-last-child(${col}) {
+          background: #fff;
+          position: sticky !important;
+          right: 0;
+          z-index: 2;
+        }
+
+        .data-table tr:not(.v-data-table__mobile-table-row):hover > td:nth-last-child(${col}),
+        .data-table tr thead:not(.v-data-table-header-mobile):hover > th:nth-last-child(${col}) {
+          background: inherit;
+        }
+
+        .data-table thead:not(.v-data-table-header-mobile) > tr > th:nth-last-child(${col}) {
+          z-index: 3 !important;
+        }
+      `),
+      ].join(' ')
     },
   },
   methods: {
