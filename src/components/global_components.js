a4e1f217368a4e9b6ab0cce7319d0bf609781b62
tanghongxin <hongxin.tang@hotmail.com>
2021-07-15 12:46:55 +0800

    🔥 remove unmaintained components

--- src/components/global_components.js
+++ src/components/global_components.js
@@ -3,13 +3,11 @@ import { DataTable } from './DataTable'
 import { FormDrawer } from './FormDrawer'
 import { Toast } from './Toast'
 import { TMap, TMarker, TSearch } from './TMap'
-import { VDateTimeInput, VDateTimePicker, VFollowMenu, VImgUpload, VLoading } from './VImplements'
+import { VFollowMenu, VImgUpload, VLoading } from './VImplements'
 import { CssStyle } from './CssStyle'
 
 Vue.component(DataTable.name, DataTable)
 Vue.component(FormDrawer.name, FormDrawer)
-Vue.component(VDateTimeInput.name, VDateTimeInput)
-Vue.component(VDateTimePicker.name, VDateTimePicker)
 Vue.component(VFollowMenu.name, VFollowMenu)
 Vue.component(VImgUpload.name, VImgUpload)
 Vue.component(VLoading.name, VLoading)
