7589bb820bf863a93b5a46058bb27555b526bee6
tanghongxin <hongxin.tang@hotmail.com>
2022-04-14 11:15:48 +0800

    ✅ (request) setup unit tests

---
+++ tests/unit/utils/request.spec.js
@@ -0,0 +1,61 @@
+import request from '@/utils/request'
+import Adaptor from 'axios-mock-adapter'
+import { createWrapper, mount } from '@vue/test-utils'
+import Vuetify from 'vuetify'
+import { Toast } from '@/components/Toast'
+
+jest.mock('@/store', () => ({
+  default: {
+    state: {
+      account: {
+        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOiIyMDIxLTEwLTI0IDAwOjAwOjAwIiwibmFtZSI6InRvZ2V0dG95b3UifQ.XdF46NflSUjnt-adAc6rNZEXI1OD6nxtwGuhz9qkxUA',
+      },
+    },
+  },
+}))
+
+describe('request', () => {
+  let store
+  let adaptor
+  let wrapper
+  let toastWrapper
+
+  beforeEach(() => {
+    store = require('@/store').default
+    wrapper = createWrapper(document.body)
+    toastWrapper = mount(Toast, {
+      vuetify: new Vuetify(),
+      attachTo: document.body,
+    })
+    adaptor = new Adaptor(request, { delayResponse: 300 })
+  })
+
+  afterEach(() => {
+    toastWrapper.destroy()
+    adaptor.reset()
+  })
+
+  it('Show toast according to error code', (done) => {
+    Promise.all(
+      [
+        [404, '资源未找到'],
+        [403, '操作被禁止'],
+        [401, '暂无权限'],
+        [500, '服务器异常'],
+      ].map(
+        async ([code, message]) => {
+          adaptor.onPost(new RegExp(code)).reply(() => [code])
+          await request.post(`/${code}`).catch(() => {})
+          const messageWrapper = wrapper.findAll('.v-snack__content').at(-1)
+          expect(messageWrapper.exists()).toBe(true)
+          expect(messageWrapper.text()).toBe(message)
+        }),
+    ).then(() => done())
+  })
+
+  it('Add jwt in request header', async () => {
+    adaptor.onPost(/test/).reply(() => [200])
+    const { config } = await request.post('/test')
+    expect(config.headers.Authorization).toEqual(`Bearer ${store.state.account.token}`)
+  })
+})
