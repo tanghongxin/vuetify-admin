665e370f5527bc9c7ac4cb9875c1325c00ff11f4
tanghongxin <hongxin.tang@hotmail.com>
2024-04-26 16:31:32 +0800

    🚸 首屏 loading 优化

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,22 @@
   <v-app id="v-application" class="fill-width fill-height">
     <router-view v-slot="{ Component }">
       <v-fade-transition leave-absolute mode="out-in">
-        <component :is="Component" />
+        <component :is="Component" @vue:mounted="removeSpinner" />
       </v-fade-transition>
     </router-view>
     <Toast />
   </v-app>
 </template>
 
-<script>
-export default {
-  name: 'App',
+<script setup lang="ts">
+defineOptions({ name: 'App' });
+
+let el = document.getElementById('app_spinner_wrapper');
+
+const removeSpinner = () => {
+  if (el) {
+    el.parentElement.removeChild(el);
+    el = null;
+  }
 };
 </script>
