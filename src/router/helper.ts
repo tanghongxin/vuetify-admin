57afbcabdb2d759311343fd6e5810b993309f594
tanghongxin <hongxin.tang@hotmail.com>
2024-04-26 17:50:09 +0800

    💚 router-view 生产环境下展示空白

--- src/router/helper.ts
+++ src/router/helper.ts
@@ -1,9 +1,9 @@
 const modules = import.meta.glob('../views/**/*.vue');
 
-export const dynamicImport = async (path: string, compName?: string) => {
-  const comp: any = await modules[`../views/${path}.vue`]();
-  if (compName) {
-    comp.default.name = compName;
-  }
-  return comp;
+export const dynamicImport = (path: string, compName?: string) => {
+  return async () => {
+    const comp: any = await modules[`../views/${path}.vue`]();
+    if (compName) comp.default.name = compName;
+    return comp;
+  };
 };
