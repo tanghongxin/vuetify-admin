a90d9928e98dd9734d77775687d244481925fc59
tanghongxin <hongxin.tang@hotmail.com>
2024-05-09 22:20:12 +0800

    ♻️ v-keep-scroll 拆分为独立 NPM 包

--- src/plugins/directives/index.ts
+++ src/plugins/directives/index.ts
@@ -1,3 +1,8 @@
-import plugin from '@/directives';
+import { Plugin } from 'vue';
+import vKeepScroll from 'v-keep-scroll';
+
+const plugin: Plugin = function (app) {
+  app.use(vKeepScroll);
+};
 
 export default plugin;
